eaa56c522368136eef91cba61b4939d5bd6eef44
Nafie <warrior259@gmail.com>
2021-11-25 21:45:50 +0300

    add is_csv()

--- app/services.py
+++ app/services.py
@@ -1,6 +1,8 @@
 async def is_csv(file_name: str) -> bool:
-    file_extention = file_name.split(".")[1]
+    """check if given file is a csv or not"""
 
+    file_extention = file_name.split(".")[1]
+    
     if file_extention == "csv":
         return True
     
