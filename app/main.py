798c01c0b715b16a383e6838ba3fc1e0a6fec36d
Nafie <warrior259@gmail.com>
2021-11-25 23:56:42 +0300

    add background task to handle uploaded file

--- app/main.py
+++ app/main.py
@@ -6,18 +6,6 @@ from app import services
 app = FastAPI()
 
 @app.post("/upload")
-async def upload(file: UploadFile = File(...)):
-    if not await services.is_csv(file.filename):
-        return {"msg": "err : not a csv file"}
-    
-    df = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), encoding='utf-8')
-    
-    # look for 'customer_average_rating' and 'product_name' in the columns
-    if "product_name" not in list(df.columns):
-        return {"msg": "err : could not find 'product_name' column"}
-    elif "customer_average_rating" not in list(df.columns):
-        return {"msg": "err : could not find 'customer_average_rating' column"}
-    
-    top_products = df[df['customer_average_rating'] >= df['customer_average_rating'].max()]
-    
-    return {"top_products": top_products.to_dict()}
+async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    background_tasks.add_task(services.handle_uploaded_file, file)
+    return {"message": "file received, processing your file..."}
