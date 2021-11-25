280e2bf66c493f56c207ec260dd8d1448b2b044d
Nafie <warrior259@gmail.com>
2021-11-25 21:46:54 +0300

    check if csv before reading

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from io import StringIO
 import pandas as pd
+from app import services
+
 app = FastAPI()
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
+    if not await services.is_csv(file.filename):
+        return {"msg": "err : not a csv file"}
     
     df = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), encoding='utf-8')
     
+    if "customer_average_rating" not in list(df.columns):
+        return {"msg": "err : could not find 'customer_average_rating' column"}
     return {"msg": file.filename}
