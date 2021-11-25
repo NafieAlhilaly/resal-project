3ce35a6150d3d0c5457b62780796b1e893d4c02e
Nafie <warrior259@gmail.com>
2021-11-25 22:08:57 +0300

    add check for pruduct_name in coluns

--- app/main.py
+++ app/main.py
@@ -13,6 +13,6 @@ async def upload(file: UploadFile = File(...)):
     df = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), encoding='utf-8')
     
     # look for 'customer_average_rating' in the columns
-    if "customer_average_rating" not in list(df.columns):
-        return {"msg": "err : could not find 'customer_average_rating' column"}
+    if "customer_average_rating" not in df.columns and "product_name" not in df.columns :
+        return {"msg": "err : could not find 'customer_average_rating' or 'product_name' column"}
     return {"msg": file.filename}
