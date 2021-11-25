a5a00920cde33580d45c3bf66664d8088982cb68
Nafie <warrior259@gmail.com>
2021-11-25 22:47:38 +0300

    add top products logic

--- app/main.py
+++ app/main.py
@@ -12,7 +12,12 @@ async def upload(file: UploadFile = File(...)):
     
     df = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), encoding='utf-8')
     
-    # look for 'customer_average_rating' in the columns
-    if "customer_average_rating" not in df.columns and "product_name" not in df.columns :
-        return {"msg": "err : could not find 'customer_average_rating' or 'product_name' column"}
-    return {"msg": file.filename}
+    # look for 'customer_average_rating' and 'product_name' in the columns
+    if "product_name" not in list(df.columns):
+        return {"msg": "err : could not find 'product_name' column"}
+    elif "customer_average_rating" not in list(df.columns):
+        return {"msg": "err : could not find 'customer_average_rating' column"}
+    
+    top_products = df[df['customer_average_rating'] >= df['customer_average_rating'].max()]
+    
+    return {"top_products": top_products.to_dict()}
