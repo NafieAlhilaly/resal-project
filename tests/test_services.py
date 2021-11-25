05dfde26063578b47d8a37a4fdc5f2a806c7e316
Nafie <warrior259@gmail.com>
2021-11-26 00:24:45 +0300

    add test_is_not_csv

--- tests/test_services.py
+++ tests/test_services.py
@@ -2,8 +2,16 @@ from app import services
 
 def test_is_csv():
     """ 
-    test if the given file extension is 'csv' or not
+    test if the given file is csv
     """
 
     file: str = "awesome_file_name.csv"
     assert services.is_csv(file) == True
+
+def test_is_not_csv():
+    """ 
+    test if the given file is not csv
+    """
+
+    file: str = "file_name.pdf"
+    assert services.is_csv(file) == False
