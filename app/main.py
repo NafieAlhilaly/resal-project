5dfef5d0ddfbb8ee54a78bc4013468b7e23b2047
Nafie <warrior259@gmail.com>
2021-11-27 01:46:27 +0300

    add handle media type check  in upload ednpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, File, UploadFile, BackgroundTasks
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks, HTTPException
 from app import services
 
 app = FastAPI()
 
 @app.post("/upload")
 async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    if file.filename.split(".")[1] != "csv":
+        raise HTTPException(status_code=415, detail="Unsupported media type")
     background_tasks.add_task(services.handle_uploaded_file, file.file.read(), file.filename, True)
     return {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
