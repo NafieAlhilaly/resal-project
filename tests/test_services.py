dc5a190758b416a9ed894f4aa5e936a924733341
Nafie <warrior259@gmail.com>
2021-11-26 12:01:27 +0300

    add test uploaded file with duplicates

--- tests/test_services.py
+++ tests/test_services.py
@@ -18,8 +18,23 @@ def test_is_not_csv():
     assert services.is_csv(file) == False
 
 def test_handle_uploaded_file_no_duplicates():
+    """
+    test uploaded file without duplicated data(same rating)
+    """
+
     with open("tests/sample data files/sample_data.csv") as file:
         assert services.handle_uploaded_file(
             file, 
             os.path.basename(file.name), 
             False) == {'top_products': {'customer_average_rating': {0: 5.0}, 'id': {0: 123}, 'product_name': {0: 'Massoub gift card'}}}
+
+def test_handle_uploaded_file_with_duplicates():
+    """
+    test uploaded file with duplicated data(same rating)
+    """
+
+    with open("tests/sample data files/sample_data_duplicates.csv") as file:
+        assert services.handle_uploaded_file(
+            file, 
+            os.path.basename(file.name), 
+            False) == {'top_products': {'customer_average_rating': {0: 5.0, 1: 5.0}, 'id': {0: 126, 1: 123}, 'product_name': {0: 'Arekah gift card', 1: 'Massoub gift card'}}}
