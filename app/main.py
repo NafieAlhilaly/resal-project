eb0f0e3efa1baeb04a97b87ad598403e4ebcce1d
Nafie <warrior259@gmail.com>
2021-11-26 20:02:00 +0300

    change message

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 @app.post("/upload")
 async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     background_tasks.add_task(services.handle_uploaded_file, file.file.read(), file.filename, True)
-    return {"message": "file received, processing your file..."}
+    return {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
