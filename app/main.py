706a58972f02863168554aa2348c955bdf2d209d
Nafie <warrior259@gmail.com>
2021-11-27 16:27:40 +0300

    remove filename param

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)
     if not services.is_csv(file.filename):
         raise HTTPException(status_code=415, detail="Not a csv file")
         
-    background_tasks.add_task(services.handle_file, file.file.read(), file.filename, True)
+    background_tasks.add_task(services.handle_file, file.file.read(), True)
     return {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
     
