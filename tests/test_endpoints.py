49aa4c97393111e5f000aba3963bde2ff268f5e4
Nafie <warrior259@gmail.com>
2021-11-27 14:08:14 +0300

    edit test_upload_csv name and docstring

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,11 +1,15 @@
 from app import main
+import os
 from fastapi.testclient import TestClient
 
 client = TestClient(main.app)
 
-def test_upload():
-    """Test upload endpoint if status_code is 200(ok)"""
+def test_upload_csv():
+    """Test upload endpoint if file is recieved abd it is a csv file"""
+    file = open("tests/sample data files/sample_data.csv")
+    
+    response = client.post("/upload", files={"file": (os.path.basename(file.name), file, "text/csv")})
 
-    response = client.post("/upload")
     response.status_code == 202
-    response.json() == {"message": "file received, processing your file..."}
+    response.json() == {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
+
