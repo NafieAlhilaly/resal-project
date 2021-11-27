26d5b8cee30e0190c88e7ea25316f07c9bc7d722
Nafie <warrior259@gmail.com>
2021-11-27 20:45:41 +0300

    add return type hint to test_endpoints.py functions

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 
 client = TestClient(main.app)
 
-def test_upload_csv():
+def test_upload_csv() -> None:
     """Test upload endpoint if file is recieved and it is a csv file"""
     file = open("tests/sample data files/sample_data.csv")
     
@@ -13,7 +13,7 @@ def test_upload_csv():
     response.status_code == 202
     response.json() == {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
 
-def test_upload_not_csv():
+def test_upload_not_csv() -> None:
     """Test upload endpoint if file is recieved and not a csv file"""
     file = open("tests/__init__.py")
     
