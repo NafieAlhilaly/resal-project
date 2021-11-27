39aa4d962736ce03524841d8712535fee4ecbf1a
Nafie <warrior259@gmail.com>
2021-11-27 13:28:22 +0300

    add check is_csv in upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, File, UploadFile, BackgroundTasks
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks, HTTPException
 from app import services
 
 app = FastAPI()
 
 @app.post("/upload", status_code=202)
 async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    if not services.is_csv(file.filename):
+        raise HTTPException(status_code=415, detail="Not a csv file")
+        
     background_tasks.add_task(services.handle_file, file.file.read(), file.filename, True)
     return {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
+    
