c68325d14f6944fb5777777ab8a2f60187c96333
Nafie <warrior259@gmail.com>
2021-11-27 14:11:27 +0300

    add test_upload_not_csv

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -5,7 +5,7 @@ from fastapi.testclient import TestClient
 client = TestClient(main.app)
 
 def test_upload_csv():
-    """Test upload endpoint if file is recieved abd it is a csv file"""
+    """Test upload endpoint if file is recieved and it is a csv file"""
     file = open("tests/sample data files/sample_data.csv")
     
     response = client.post("/upload", files={"file": (os.path.basename(file.name), file, "text/csv")})
@@ -14,7 +14,7 @@ def test_upload_csv():
     response.json() == {"message": "file received, processing your file, you'll be notified when we finish proccessing"}
 
 def test_upload_not_csv():
-    """Test upload endpoint if file is recieved abd it is a csv file"""
+    """Test upload endpoint if file is recieved and not a csv file"""
     file = open("tests/__init__.py")
     
     response = client.post("/upload", files={"file": (os.path.basename(file.name), file, "text/csv")})
